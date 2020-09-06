7929de80c03b535348fe0446e9fdfaec2c588c78
Que <hoang.newking@gmail.com>
2020-09-06 16:02:15 +0700

    completed modal

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,16 @@
+import '../styles/styles.css'
+import MobileMenu from './modules/MobileMenu'
+import RevealOnScroll from './modules/RevealOnScroll'
+import StickyHeader from './modules/StickyHeader'
+import Modal from './modules/Modal'
+
+new Modal()
+let stickyHeader = new StickyHeader()
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
+
+let mobileMenu = new MobileMenu();
+
+if (module.hot) {
+  module.hot.accept()
+}
