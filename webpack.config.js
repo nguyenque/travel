16210d43177ae7ed5ffc4de7f6fef2091aa8a457
Que <hoang.newking@gmail.com>
2020-09-07 15:37:33 +0700

    part-1 of preparing website go to live

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const currentTask = process.env.npm_lifecycle_event
 const path = require('path')
 
 const postCSSPlugins = [
@@ -9,28 +10,46 @@ const postCSSPlugins = [
   require('autoprefixer')
 ]
 
-module.exports = {
+let config = {
   entry: './app/assets/scripts/App.js',
-  output: {
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader?url=false', { loader: 'postcss-loader', options: { plugins: postCSSPlugins } }]
+      }
+    ]
+  }
+}
+
+if(currentTask == 'dev') {
+  config.output = {
     filename: 'bundled.js',
     path: path.resolve(__dirname, 'app')
-  },
-  devServer: {
-    before: function(app, server) {
+  }
+
+  config.devServer = {
+    before: function (app, server) {
       server._watch('./app/**/*.html')
     },
     contentBase: path.join(__dirname, 'app'),
     hot: true,
     port: 3000,
     host: '0.0.0.0'
-  },
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
-      }
-    ]
   }
+
+  config.mode = 'development'
 }
+if(currentTask == 'build') {
+  config.output = {
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+  
+  config.mode = 'production'
+}
+
+
+
+
+module.exports = config
